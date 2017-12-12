5a6fbc5b6f5807949afc0d24233c870ba193175f
Zachary Clinton Jones <zajonesck83@gmail.com>
2017-12-12 01:01:07 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,9 +1,9 @@
 var index = require("./index.js")
 
-if (index.name === "Susan") {
+if (index.name === "Jane") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "+index.name)
+    console.log("Expected: Jane, Received: "+index.name)
 }
 if (index.height === "70") {
     console.log("The height is correct")
